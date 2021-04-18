47803b41a792bed5bb0df924af3f645ba68ea4fe
Semih Boyadzha <sboyadzha@student.methodist.edu>
2021-04-18 12:48:42 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID = "47db332a-bb44-4bf5-826e-2a6db9add65f"
+            userName = "admin"
+            userSecret = "123123"
+
+
+        />
+    )
+}
+
+export default App;
